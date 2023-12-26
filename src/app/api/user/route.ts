2176c0c190277517174c32ddf0ca79e221a3f292
nono <nono.polkadot@gmail.com>
2023-12-26 16:53:01 +0800

    api(user)!: add zod

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,23 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 import { hash } from 'bcrypt';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const userSchema = z
+  .object({
+    username: z.string().min(1, 'Username is required').max(100),
+    email: z.string().min(1, 'Email is required').email('Invalid email'),
+    password: z
+      .string()
+      .min(1, 'Password is required')
+      .min(8, 'Password must have than 8 characters'),
+  });
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { email, username, password } = body;
+    const { email, username, password } = userSchema.parse(body);
 
     const existingUserEmail = await db.user.findUnique({
       where: {email: email}
